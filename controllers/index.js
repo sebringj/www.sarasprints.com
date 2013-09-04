e24207f2b8901ddc084375c0eac3a2fc9ab69e06
streampunch <geneyoo@new-host-5.home>
2013-09-04 12:09:57 -0700

    update

--- controllers/index.js
+++ controllers/index.js
@@ -6,4 +6,31 @@ module.exports.set = function(app) {
 			title : "Sara's Prints"
 		});
 	});
+	app.get('/contactus', function(req, res){
+		res.render('contactus', {
+			year : year,
+			title : "Contact Us"
+		});
+	});
+	app.get(/^\/(team|story|sizing-chart|site-map)$/, function(req, res) {
+		
+		// get data from kitgui...
+		/*
+		kitgui.get([
+			{id:'title',type:'inline'},
+			{id:'body',type:'html'}
+		], function(){
+			res.render('content', {
+				year : year,
+				title : title,
+				body : body
+			});
+		});
+		*/
+		
+		res.render('content', {
+			year : year,
+			title : "template page"
+		});
+	});
 };
