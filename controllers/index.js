f433bf15bd44b8c4e07e6aaeadb981bbc846ce87
streampunch <macbookair@streampunch.home>
2013-09-15 17:06:32 -0700

    fo

--- controllers/index.js
+++ controllers/index.js
@@ -12,6 +12,20 @@ module.exports.set = function(app) {
 			title : "Contact Us"
 		});
 	});
+	app.get(/^\/(pjs-for-girls|pjs-for-boys|fuzzy-fleece|sale|up-past-8)$/, function(req, res) {
+		var products = [
+			{productName:"productone"},
+			{productName:"producttwp"},
+			{productName:"productthree"},
+			{productName:"productfour"},
+			{productName:"productfive"}
+		];
+		res.render('catalog', {
+			year : year,
+			title : "Catalog",
+			products : products
+		});
+	});
 	app.get(/^\/(team|story|sizing-chart|site-map)$/, function(req, res) {
 		
 		// get data from kitgui...
@@ -30,7 +44,22 @@ module.exports.set = function(app) {
 		
 		res.render('content', {
 			year : year,
-			title : "template page"
+			title : "template page",
+			html : ""
 		});
+	});
+	app.use(function(req, res, next){
+		res.status(404);
+		if (req.accepts("html")){
+			res.render('404', {
+				year : year,
+				title : "404 page not found"
+			});
+			return;
+		} else if (req.accepts("json")){
+			res.send({ error: 'not found'});
+			return;
+		}
+		res.type('txt').send('not found');
 	});
 };
