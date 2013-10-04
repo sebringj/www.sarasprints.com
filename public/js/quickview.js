e56ce14e79a5224c614b7607aa36a4e68d637778
adrian271 <Adrian@mba-2011.home>
2013-10-04 09:43:44 -0700

    Experimenting with SWIG - avoiding pre-compile of files

--- public/js/quickview.js
+++ public/js/quickview.js
@@ -13,8 +13,9 @@ $('head').append($('<link>',{ href: '/css/quickview.css', rel:'stylesheet' }));
 		set : function(product) {
 			var tpl = $('#quick-view-template').html();
 			var output = swig.compile(tpl,{});
-			var html = output({ locals: { product: product }});
-			$('.quick-view-modal').html(output);
+			//var output = swig.render(tpl, { filename: '/tpl', locals: { stuff: 'awesome' }});
+			var html = output(tpl,{ locals: { product: product }});
+			$('.quick-view-modal').html(html);
 			return this;
 		}
 	};
