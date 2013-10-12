6e083ea31066ddd3fdb0a68eed697eac8617b14d
adrian271 <Adrian@mba-2011.gateway.pace.com>
2013-10-12 12:39:15 -0700

    removed not from quickview.js

--- public/js/quickview.js
+++ public/js/quickview.js
@@ -22,7 +22,7 @@ $('.quick-view-modal').modal({ show : false });
 		}
 	};
 
-	if (!sessionStorage['quickview-tpl']) { // added ! to this temporarily to undo caching while working on quick view
+	if (sessionStorage['quickview-tpl']) { // added ! to this temporarily to undo caching while working on quick view
 		context.quickView.tpl = sessionStorage['quickview-tpl'];
 	} else {
 		$.ajax({
